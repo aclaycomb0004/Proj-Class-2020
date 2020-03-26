1d072c03056bbc3202713e9e6c44cc247ada920f
Alyssa Claycomb <alyssaclaycomb@hotmail.com>
2020-03-25 23:43:45 -0400

    made Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css'
+
+
+
+function Header () {
+    return (
+        <div className="header">
+            This is my header.
+        </div>
+    )
+}
+
+export default Header;
