c763052f31c007fad1ec90649ee078c2a40d8743
Alyssa Claycomb <alyssaclaycomb@hotmail.com>
2020-04-24 17:38:23 -0400

    more carousel code

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,20 +1,68 @@
-import React from 'react';
+import React from "react";
+
 
 class Carousel extends React.Component {
+
+  showSlide() {
+    const slides = document.querySelectorAll(".slide");
+    const dots = document.querySelectorAll(".dot");
+    if (currentSlide > slides.length) {
+        currentSlide = 1;
+    }
+
+    if (currentSlide < 1) {
+        currentSlide = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        const element = slides[i];
+        element.style.display = "none";
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        const el = dots[i];
+        el.classList.remove("active");
+    }
+
+    slides[currentSlide - 1].style.display = "block";
+
+    dots[currentSlide - 1].classList.add("active");
+}
+
   render() {
     return (
       <div>
-        <img src={require("./images/slide-1")} style={{height:50, width:50}} />
+        <div className="slideshow-wrapper">
+          <div className="slides-container">
+            <div className="slide fade">
+              <img src={require("./images/slide-1.png")} alt="" />
+            </div>
+            <div className="slide fade">
+              <img src={require("./images/slide-2.png")} alt="" />
+            </div>
+            <div className="slide fade">
+              <img src={require("./images/slide-3.png")} alt="" />
+            </div>
+            <div className="slide fade">
+              <img src={require("./images/slide-4.png")} alt="" />
+            </div>
+            <button className="prev">&#10094;</button>
+            <button className="next">&#10095;</button>
+          </div>
+
+          <div className="dots">
+            <span className="dot active" onclick="specificSlide(1)"></span>
+            <span className="dot" onclick="specificSlide(2)"></span>
+            <span className="dot" onclick="specificSlide(3)"></span>
+            <span className="dot" onclick="specificSlide(4)"></span>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
 export default Carousel;
-
-
-
-
 
 // console.log('working')
 // let currentSlide = 1;
@@ -64,9 +112,6 @@ export default Carousel;
 // document.querySelector(".next").addEventListener("click", increaseSlide);
 
 // showSlide();
-
-
-
 
 // <div class="slideshow-wrapper">
 //       <div class="slides-container">
