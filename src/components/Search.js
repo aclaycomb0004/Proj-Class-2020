a3966a822d97ce36d2ca039fcb63dfc7ff502e4d
Alyssa Claycomb <alyssaclaycomb@hotmail.com>
2020-05-05 22:11:51 -0400

    fetched  and mapped over search results

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,40 +1,62 @@
-import React from 'react';
-
-
-
+import React from "react";
 
 class Search extends React.Component {
+  state = {
+    searchRequest: "",
+    searchResults: [],
+  };
+  handleChange = (event) => {
+    this.setState({ searchRequest: event.target.value });
+  };
 
-    state = {
-        searchRequest: "",
-        searchResults: []
-    }
-    handleChange = (event) => {
-        this.setState({searchRequest: event.target.value});
-      }
-    
-      handleSubmit = (event) => {
-        alert('A name was submitted: ' + this.state.searchRequest);
-        event.preventDefault();
-      }
-    render() {
-        console.log("search page")
-        return (
-            <div className="SearchCont">
-                <p className="SearchByText">Search by {this.props.searchType}:</p>
-                <form className="FormCont" onSubmit={this.handleSubmit}>
-                    <label>
-                    Keyword(s):
-                        <div className="TextField">
-                            <input type="text" value={this.state.searchType} onChange={this.handleChange} />
-                        </div>
-                    </label>
-                    <input type="submit" value="Search" />
-                </form>
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.fetchBooks();
+  };
+
+  fetchBooks = async () => {
+    let keywords = this.state.searchRequest.split(" ").join("+");
+    const url = `http://openlibrary.org/search.json?title=${keywords}`;
+    const res = await fetch(url);
+    const books = await res.json();
+    this.setState({ searchResults: books });
+    console.log(books);
+  };
+
+  render() {
+    const ListResults = () => {
+      return (
+        <ul>
+          {this.state.searchResults.length &&
+            this.state.searchResults.docs.map((book) => {
+              return <li>{book.title}</li>;
+            })}
+        </ul>
+      );
+    };
+    console.log("search page", this.state);
+    return (
+      <div className="SearchCont">
+        <p className="SearchByText">Search by {this.props.searchType}:</p>
+        <form className="FormCont" onSubmit={this.handleSubmit}>
+          <label>
+            Keyword(s):
+            <div className="TextField">
+              <input
+                type="text"
+                value={this.state.searchType}
+                onChange={this.handleChange}
+              />
             </div>
-        )
-    }
-
+          </label>
+          <input type="submit" value="Search" />
+        </form>
+        <div>
+          <ListResults />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Search;
